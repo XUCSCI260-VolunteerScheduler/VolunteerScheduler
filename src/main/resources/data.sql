c24a62941660810aec74ae52cbc5025491ace6bc
Chris <staudigelc@xavier.edu>
2018-01-25 19:24:37 -0500

    database example added'

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO STUDENT(S_FNAME, S_LNAME, S_ID, S_MAJOR)
+    VALUES ('Chuck', 'Norris', '1234567', 'Feminist Dance Therapy');
--- src/test/java/io/github/xucsci260volunteerscheduler/Services/Interfaces/ServiceExampleTest.java
+++ src/test/java/io/github/xucsci260volunteerscheduler/Services/Interfaces/ServiceExampleTest.java
@@ -25,5 +25,4 @@ public class ServiceExampleTest {
             assertEquals(ArrayIndexOutOfBoundsException.class, e.getClass());
         }
     }
-
 }
