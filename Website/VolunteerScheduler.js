fe580b825278f9a14bee19b2c243398d2a03994b
gstapes <stapletong@xavier.edu>
2018-04-03 17:57:44 -0400

    changed sign up
    added javascript for sign up and added createuser instead

---
+++ Website/VolunteerScheduler.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    $("#createUserSubmitButton").click(function() {
+       var firstName = document.getElementById("firstName").value;
+       var lastName = document.getElementById("lastName").value;
+       var dob = document.getElementById("dob").value;
+       var username = document.getElementById("username").value;
+       var password = document.getElementById("password").value;
+       var email = document.getElementById("email").value;
+
+       var path = "/users/create";
+
+       $.ajax({
+           url: path,
+           type: "POST",
+           beforeSend: function(request) {
+               request.setRequestHeader("username", username);
+               request.setRequestHeader("email", email);
+               request.setRequestHeader("first", firstName);
+               request.setRequestHeader("last", lastName);
+               request.setRequestHeader("dob", dob);
+               request.setRequestHeader("password", password);
+           }
+       })
+    });
+});
