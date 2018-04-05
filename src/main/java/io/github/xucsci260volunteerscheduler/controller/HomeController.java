4049ac57049d7ba0b3d6c78afd6ee8f0b752f016
IsaacNasser <ikenasser@gmail.com>
2018-04-05 15:18:45 -0400

    Added user import to user controller

---
+++ src/main/java/io/github/xucsci260volunteerscheduler/controller/HomeController.java
@@ -0,0 +1,13 @@
+package io.github.xucsci260volunteerscheduler.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = {"/", "/home"})
+    public String goHome() {
+        return "home";
+    }
+}
