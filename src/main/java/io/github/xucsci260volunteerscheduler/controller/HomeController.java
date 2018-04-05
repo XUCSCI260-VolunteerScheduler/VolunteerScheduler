246058cf33700aeec3308014ef23f4226191d9cf
IsaacNasser <ikenasser@gmail.com>
2018-04-05 17:50:42 -0400

    Changed resources

--- src/main/java/io/github/xucsci260volunteerscheduler/controller/HomeController.java
+++ src/main/java/io/github/xucsci260volunteerscheduler/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @RequestMapping(value = {"/", "/home"})
     public String goHome() {
-        return "/src/main/resources/templates/home.html";
-        //return "home";
+        //return "/src/main/resources/templates/home.html";
+        return "home";
     }
 }
