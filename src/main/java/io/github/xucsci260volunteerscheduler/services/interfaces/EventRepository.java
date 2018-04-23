4ccb775bf64ef3f728b2cee28521be8f8862006c
IsaacNasser <ikenasser@gmail.com>
2018-04-22 23:16:01 -0400

    Revert "added Event controller and repo"
    This reverts commit 6a8c4448b6435a0c94c32a8c99e94874267871fe.

--- src/main/java/io/github/xucsci260volunteerscheduler/services/interfaces/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.xucsci260volunteerscheduler.domain;
-
-import org.springframework.data.repository.CrudRepository;
-import src/main/java/io/github/xucsci260volunteerscheduler/domain/Event.java
-
-public interface EventRepository extends CrudRepository<Event, Long> {
-
-}
