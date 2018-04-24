6ac8766f1d2b68bb34b6184445e35879355a9dc4
IsaacNasser <ikenasser@gmail.com>
2018-04-23 20:46:59 -0400

    Event Controller and DB patch

--- src/main/java/io/github/xucsci260volunteerscheduler/controller/EventController.java
+++ src/main/java/io/github/xucsci260volunteerscheduler/controller/EventController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import src/main/java/io/github/xucsci260volunteerscheduler/domain/Event.java;
-import src/main/java/io/github/xucsci260volunteerscheduler/dal/interfaces/EventRepository.java;
+import src.main.java.io.github.xucsci260volunteerscheduler.domain.Event.java;
+import src.main.java.io.github.xucsci260volunteerscheduler.dal.interfaces.EventRepository.java;
 
 @Controller
 @RequestMapping(path='/home')
@@ -17,9 +17,7 @@ public class EventController {
     private EventRepository eventRepository;
 
     @GetMapping(path='/add')
-    public @ResponseBody String addNewEvent (@RequestParam String name, @RequestParam String desc, @RequestParam String email,
-                                             @RequestParam String phone, @RequestParam String address, @RequestParam String city
-                                                @RequestParam String zip, @RequestParam String state) {
+    public @ResponseBody String addNewEvent (@RequestParam String name, @RequestParam String desc, @RequestParam String email, @RequestParam String phone, @RequestParam String address, @RequestParam String city, @RequestParam String zip, @RequestParam String state) {
         Event e = new Event();
         e.setName(name);
         e.setDesc(desc);
