5605848190fff284c1af1f27d597c5664b8c32b5
gstapes <stapletong@xavier.edu>
2018-04-24 09:08:04 -0400

    fixing import statement

--- src/main/java/io/github/xucsci260volunteerscheduler/dal/interfaces/EventRepository.java
+++ src/main/java/io/github/xucsci260volunteerscheduler/dal/interfaces/EventRepository.java
@@ -3,7 +3,7 @@ package io.github.xucsci260volunteerscheduler.dal.interfaces;
 //import org.springframework.data.repository.CrudRepository;
 //import src.main.java.io.github.xucsci260volunteerscheduler.domain.Event.java;
 import org.springframework.data.repository;
-import domain.Event;
+import io.github.xucsci260volunteerscheduler.domain.Event;
 
 public interface EventRepository extends CrudRepository<Event, Long> {
 
