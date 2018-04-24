e3d4e252c6decb2be238153fda47c1b8c1da7093
IsaacNasser <ikenasser@gmail.com>
2018-04-24 17:55:00 -0400

    MongoDB compatibility

--- src/main/java/io/github/xucsci260volunteerscheduler/dal/interfaces/EventRepository.java
+++ src/main/java/io/github/xucsci260volunteerscheduler/dal/interfaces/EventRepository.java
@@ -1,10 +1,10 @@
 package io.github.xucsci260volunteerscheduler.dal.interfaces;
 
-import org.springframework.data.repository.CrudRepository;
 //import src.main.java.io.github.xucsci260volunteerscheduler.domain.Event.java;
 //import org.springframework.data.repository;
 import io.github.xucsci260volunteerscheduler.domain.Event;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface EventRepository extends CrudRepository<Event, Long> {
+public interface EventRepository extends MongoRepository<Customer, String> {
 
 }
