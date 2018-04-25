3a74529abf178423a2662ed695776eefb3d25a64
gstapes <stapletong@xavier.edu>
2018-04-25 17:50:29 -0400

    that boy needs an import

--- src/main/java/io/github/xucsci260volunteerscheduler/dal/interfaces/EventRepository.java
+++ src/main/java/io/github/xucsci260volunteerscheduler/dal/interfaces/EventRepository.java
@@ -3,6 +3,7 @@ package io.github.xucsci260volunteerscheduler.dal.interfaces;
 //import src.main.java.io.github.xucsci260volunteerscheduler.domain.Event.java;
 //import org.springframework.data.repository;
 import io.github.xucsci260volunteerscheduler.domain.Event;
+import io.github.xucsci260volunteerscheduler.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface EventRepository extends MongoRepository<User, String> {
