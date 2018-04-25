ea869a74b3922b1c4ca5db70d07a36382f50787f
IsaacNasser <ikenasser@gmail.com>
2018-04-25 18:27:04 -0400

    spring imports

--- src/main/java/io/github/xucsci260volunteerscheduler/controller/EventController.java
+++ src/main/java/io/github/xucsci260volunteerscheduler/controller/EventController.java
@@ -2,6 +2,7 @@ package io.github.xucsci260volunteerscheduler.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import io.github.xucsci260volunteerscheduler.domain.Event;
 import io.github.xucsci260volunteerscheduler.dal.interfaces.EventRepository;
 import java.util.List;
+
+
 
 @Controller
 @RequestMapping(path="/home")
--- src/main/java/io/github/xucsci260volunteerscheduler/dal/interfaces/EventRepository.java
+++ src/main/java/io/github/xucsci260volunteerscheduler/dal/interfaces/EventRepository.java
@@ -7,6 +7,7 @@ import io.github.xucsci260volunteerscheduler.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
+
 public interface EventRepository extends MongoRepository<User, String> {
     public List<Event> findAll(){
 
