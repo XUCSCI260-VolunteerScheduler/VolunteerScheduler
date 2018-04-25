7696b82fa5a79d0ae59dd8b01be20de1f6ff77c1
IsaacNasser <ikenasser@gmail.com>
2018-04-25 18:30:55 -0400

    patch #8675309

--- src/main/java/io/github/xucsci260volunteerscheduler/dal/interfaces/EventRepository.java
+++ src/main/java/io/github/xucsci260volunteerscheduler/dal/interfaces/EventRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 
-public interface EventRepository extends MongoRepository<User, String> {
+public interface EventRepository extends MongoRepository<Event, String> {
     public List<Event> findAll(){
 
     }
