8b4a4630f0c6e762acb07c9b39d04bc2ac104c3c
IsaacNasser <ikenasser@gmail.com>
2018-04-26 05:45:45 -0400

    and back

--- src/main/java/io/github/xucsci260volunteerscheduler/dal/interfaces/EventRepository.java
+++ src/main/java/io/github/xucsci260volunteerscheduler/dal/interfaces/EventRepository.java
@@ -5,14 +5,14 @@ package io.github.xucsci260volunteerscheduler.dal.interfaces;
         import io.github.xucsci260volunteerscheduler.domain.Event;
         import io.github.xucsci260volunteerscheduler.domain.User;
         import org.springframework.data.mongodb.repository.MongoRepository;
-        import org.springframework.data.repository.CrudRepository;
+        //import org.springframework.data.repository.CrudRepository;
         import java.util.List;
 
-public interface EventRepository extends CrudRepository {
-    public List<Event> findAll();
-}
-
-
-//public interface EventRepository extends MongoRepository<Event, String> {
-//
+//public interface EventRepository extends CrudRepository {
+//    public List<Event> findAll();
 //}
+
+
+public interface EventRepository extends MongoRepository<Event, String> {
+        public List<Event> findAll();
+}
