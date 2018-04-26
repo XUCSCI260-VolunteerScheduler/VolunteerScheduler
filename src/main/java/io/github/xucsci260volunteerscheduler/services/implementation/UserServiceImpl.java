da7ccf20778fd68dbc7302237e08ac73fab858ff
gstapes <stapletong@xavier.edu>
2018-04-26 04:37:19 -0400

    anything?

--- src/main/java/io/github/xucsci260volunteerscheduler/services/implementation/UserServiceImpl.java
+++ src/main/java/io/github/xucsci260volunteerscheduler/services/implementation/UserServiceImpl.java
@@ -44,7 +44,7 @@ public class UserServiceImpl implements UserService {
      */
     @Override
     public CreateUserResponse createUser(CreateUserRequest request) {
-        GetUserRequest getRequest = new GetUserRequest(GetMethodEnum.USERNAME, request.getUser().getUsername(),  "");
+        GetUserRequest getRequest = new GetUserRequest(GetMethodEnum.USERNAME, request.getUser(),  "");
         CreateUserResponse response;
         User u = userDAO.getUser(getRequest);
 
