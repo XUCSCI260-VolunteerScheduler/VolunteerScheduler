1ab640e6848773612ba3c748f5886ee39d41f806
gstapes <stapletong@xavier.edu>
2018-04-25 21:01:18 -0400

    welcome back to nothing

--- src/main/java/io/github/xucsci260volunteerscheduler/VolunteerSchedulerApplication.java
+++ src/main/java/io/github/xucsci260volunteerscheduler/VolunteerSchedulerApplication.java
@@ -5,11 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VolunteerSchedulerApplication {
-
-    @RequestMapping(value = {"/", "/home"})
-    public String home(){
-      return "home";
-    }
     public static void main(String[] args) {
         SpringApplication.run(VolunteerSchedulerApplication.class, args);
     }
