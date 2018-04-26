8b0737f1e22c9daa9d7c6c9e253d1df7e1762b0e
gstapes <stapletong@xavier.edu>
2018-04-25 21:06:49 -0400

    imports

--- src/main/java/io/github/xucsci260volunteerscheduler/VolunteerSchedulerApplication.java
+++ src/main/java/io/github/xucsci260volunteerscheduler/VolunteerSchedulerApplication.java
@@ -2,6 +2,8 @@ package io.github.xucsci260volunteerscheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
 @SpringBootApplication
 public class VolunteerSchedulerApplication {
