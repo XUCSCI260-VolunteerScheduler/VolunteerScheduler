2e8a030fb8d39ee7095bb29f0bb950e55ad5e589
IsaacNasser <ikenasser@gmail.com>
2018-04-26 05:40:42 -0400

    mongod to crud

--- src/main/java/io/github/xucsci260volunteerscheduler/dal/interfaces/EventRepository.java
+++ src/main/java/io/github/xucsci260volunteerscheduler/dal/interfaces/EventRepository.java
@@ -2,12 +2,17 @@ package io.github.xucsci260volunteerscheduler.dal.interfaces;
 
 //import src.main.java.io.github.xucsci260volunteerscheduler.domain.Event.java;
 //import org.springframework.data.repository;
-import io.github.xucsci260volunteerscheduler.domain.Event;
-import io.github.xucsci260volunteerscheduler.domain.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
+        import io.github.xucsci260volunteerscheduler.domain.Event;
+        import io.github.xucsci260volunteerscheduler.domain.User;
+        import org.springframework.data.mongodb.repository.MongoRepository;
+        import org.springframework.data.repository.CrudRepository;
+        import java.util.List;
 
-
-public interface EventRepository extends MongoRepository<Event, String> {
+public interface EventRepository extends CrudRepository {
     public List<Event> findAll();
 }
+
+
+//public interface EventRepository extends MongoRepository<Event, String> {
+//
+//}
